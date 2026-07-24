cb46426a26ee3ebf455aed5eadcb0bc02a46f7b2
Peter Wijntjes BB <peter@bizzbrain.nl>
2026-07-24 18:35:48 +0200

    incremental make

--- internal/cbm/vendored/grammars/calnav/parser.c
+++ internal/cbm/vendored/grammars/calnav/parser.c
@@ -7792,7 +7792,7 @@ extern "C" {
 #define TS_PUBLIC __attribute__((visibility("default")))
 #endif
 
-TS_PUBLIC const TSLanguage *tree_sitter_cal(void) {
+TS_PUBLIC const TSLanguage *tree_sitter_calnav(void) {
   static const TSLanguage language = {
     .abi_version = LANGUAGE_VERSION,
     .symbol_count = SYMBOL_COUNT,
@@ -7817,7 +7817,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_cal(void) {
     .lex_modes = (const void*)ts_lex_modes,
     .lex_fn = ts_lex,
     .primary_state_ids = ts_primary_state_ids,
-    .name = "cal",
+    .name = "calnav",
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
